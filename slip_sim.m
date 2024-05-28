cf10d667685facf274a65402cf19c236fddc71b9
silvery107 <yulunz@umich.edu>
2024-05-28 12:06:02 -0400

    Calc feedback gain via finite diff

--- slip_sim.m
+++ slip_sim.m
@@ -11,58 +11,116 @@ p.vx_des = 1.;
 p.num_jump = 5;
 p.max_phase_dt = 1;
 p.Ts = 0.2;
-p.ctrl_flag = 1; % 1 for Raibert heuristic, 2 for periodic gait library
-
+p.ctrl_flag = 2;
 p.plot = false;
-[x_star, u_star] = find_periodic_gait(p.vx_des, p)
+
+[x_apex_star, u_star] = find_periodic_gait(p.vx_des, p)
+K = calc_feedback_gain(x_apex_star, u_star, p)
 
 u0 = u_star;
-pc0 = [0; x_star(1)];
+pc0 = [0; x_apex_star(1)];
 dpc0 = [p.vx_des; 0];
 pf0 = pc0 + rot_mat(u0) * [0; -p.l0];
 x0 = [pc0; dpc0; pf0]; 
 t0 = 0;
-
 % x: [pc_x, pc_z, dpc_x, dpc_z, pf_x, pf_z]
-% u: [th]
-
-x = x0;
-t = t0;
-u = u0;
+% u: [th] % TODO include Ks as control?
 
 p.plot = true;
 p.fig = figure();
 % exportgraphics(p.fig, "my_slip.gif", "Append", false);
+
+x = x0;
+t = t0;
+u = u0;
+%% Main loop
 for ii=1:p.num_jump
-    % Apply control
-    % u = controller(x, p);
-    u = u_star; % TODO snap state to gait library and apply control
+    % Unpack states
     pc = x(1:2);
-    x(5:6) = pc + rot_mat(u) * [0; -p.l0];
+    dpc = x(3:4);
+    x_apex = x(2:3);
+    % Calculate control input
+    switch p.ctrl_flag
+        case 1 % Raibert heuristic
+            pfx_err = dpc(1) * p.Ts / 2 + p.kr * (dpc(1) - p.vx_des);
+            u = asin(max(min(pfx_err, p.l0), -p.l0) / p.l0);
+        case 2 % Linear deadbeat
+            u = u_star + K * (x_apex - x_apex_star);
+        case 3 % Open loop
+            u = u_star;
+    end
+    % Apply control
+    pf_tgt = pc + rot_mat(u) * [0; -p.l0];
+    x(5:6) = pf_tgt;
     % Simulate apex to apex
     [t, x] = simulate(t, x, p);
-    % Earily return
+    % Early return
     if x(6) < -1e-5 || x(2) < -1e-5
         return;
     end
 end
 
-function [x_star, u_star] = find_periodic_gait(vx, p)
-    rho_init = [1; 0]; % pc_z, u
-    rho = lsqnonlin(@(rho)min_apex_map(rho(1), rho(2), vx, p), rho_init);
-    x_star = [rho(1); vx];
-    u_star = rho(2);
+%% Functions
+function build_gait_library(p)
+    % TODO build a search tree and map x_apex to (K, x*, u*) tuple
 end
 
-function obj = min_apex_map(pc_z, u, vx, p)
+function K = calc_feedback_gain(x_star, u_star, p)
+    % Calculate K matrix via centered finite difference
+    epsilon = 1e-4;
+    dim_x = length(x_star);
+    dim_u = length(u_star);
+
+    Jx = zeros(dim_x, dim_x);
+    for idx=1:size(Jx, 2)
+        x_perturb = x_star;
+        x_perturb(idx) = x_star(idx) - epsilon;
+        x_prev = apex_to_apex_map(x_perturb, u_star, p);
+        x_perturb = x_star;
+        x_perturb(idx) = x_star(idx) + epsilon;
+        x_next = apex_to_apex_map(x_perturb, u_star, p);
+        Jx(:, idx) = (x_next - x_prev) / 2*epsilon;
+    end
+
+    Ju = zeros(dim_x, dim_u);
+    for idx=1:size(Ju, 2)
+        u_perturb = u_star;
+        u_perturb(idx) = u_star(idx) - epsilon;
+        x_prev = apex_to_apex_map(x_star, u_perturb, p);
+        u_perturb = u_star;
+        u_perturb(idx) = u_star(idx) + epsilon;
+        x_next = apex_to_apex_map(x_star, u_perturb, p);
+        Ju(:, idx) = (x_next - x_prev) / 2*epsilon;
+    end
+
+    K = -Ju\Jx;
+end
+
+function [x_star, u_star] = find_periodic_gait(vx, p)
+    var_init = [1; 0]; % opt var: pc_z, u
+    var = lsqnonlin(@(var)min_apex_map(var, vx, p), var_init);
+    x_star = [var(1); vx];
+    u_star = var(2);
+end
+
+function obj = min_apex_map(var, vx, p)
+    % min_{pc_z, u} ||x_apex_next - x_apex||^2
+    pc_z = var(1);
+    u = var(2);
+    x_apex = [pc_z; vx];
+    x_apex_next = apex_to_apex_map(x_apex, u, p);
+    obj = x_apex_next - x_apex;
+end
+
+function x_apex_next = apex_to_apex_map(x_apex, u, p)
+    pc_z = x_apex(1);
+    vx = x_apex(2);
     pc0 = [0; pc_z];
     dpc0 = [vx; 0];
     pf0 = pc0 + rot_mat(u) * [0; -p.l0];
     x0 = [pc0; dpc0; pf0]; 
     [~, x_next] = simulate(0, x0, p);
-    x_apex = [x_next(2); x_next(3)];
-
-    obj = x_apex - [pc_z; vx];
+    x_apex_next = [x_next(2); x_next(3)];
 end
 
 function [t0, x] = simulate(t0, x, p)
@@ -104,16 +162,6 @@ function [t0, x] = simulate(t0, x, p)
 
     for idx=1:length(sol.x)
         plot_robot(sol.y(:, idx), p);
-    end
-end
-
-function u = controller(x, p)
-    dpc = x(3:4);
-    if p.ctrl_flag==1
-        pfx_err = dpc(1) * p.Ts / 2 + p.kr * (dpc(1) - p.vx_des);
-        u = asin(max(min(pfx_err, p.l0), -p.l0) / p.l0);
-    else
-        error("NotImplementedError")
     end
 end
 
